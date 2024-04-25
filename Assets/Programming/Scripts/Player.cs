b72edc173d446e4a81f4d18343192755e591a0a1
Vahalcadi <davidepetrone04@gmail.com>
2024-04-25 22:25:41 +0200

    Attack animation test

--- Assets/Programming/Scripts/Player.cs
+++ Assets/Programming/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
     private Vector2 vector;
     private Vector3 input;
     private InputManager inputManager;
+
+    [SerializeField] private Animator anim;
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed;
     [SerializeField] private float turnSpeed;
@@ -20,7 +22,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         //vector = inputManager.Movement().normalized;
-        
+
+        if (inputManager.Attack())
+            AttackAnimationTest();
+
         input = new Vector3(inputManager.Movement().normalized.x, 0, inputManager.Movement().normalized.y);
         Look();
     }
@@ -52,5 +57,10 @@ public class Player : MonoBehaviour
     private void Movement()
     {
         rb.MovePosition(transform.position + (transform.forward * input.normalized.magnitude) * speed * Time.deltaTime);
+    }
+
+    private void AttackAnimationTest()
+    {
+        anim.SetTrigger("isAttacking");
     }
 }
