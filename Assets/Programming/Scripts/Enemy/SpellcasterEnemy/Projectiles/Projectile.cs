751ed637127824bffb74d2a7f13336874514bc2a
Davide Petrone <davidepetrone04@gmail.com>
2024-05-20 09:48:56 +0200

    adjusted projectiles

--- Assets/Programming/Scripts/Enemy/SpellcasterEnemy/Projectiles/Projectile.cs
+++ Assets/Programming/Scripts/Enemy/SpellcasterEnemy/Projectiles/Projectile.cs
@@ -4,10 +4,20 @@ public class Projectile : MonoBehaviour
 {
     [SerializeField] private Rigidbody rb;
     [SerializeField] private float speed;
+    [SerializeField] private float despawnTime;
+    private float timer;
     public int Damage { get; set; }
+    private void Start()
+    {
+        timer = despawnTime;
+    }
     private void Update()
     {
+        timer -= Time.deltaTime;
         rb.velocity = transform.forward * speed;
+
+        if (timer < 0)
+            Destroy(gameObject);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,10 +26,8 @@ public class Projectile : MonoBehaviour
         {
             Player player = other.GetComponent<Player>();
 
-            if(!player.IsInvincible)
+            if (!player.IsInvincible)
                 player.TakeDamage(Damage);
         }
-        if (!other.CompareTag("Enemy"))
-            Destroy(gameObject);
     }
 }
